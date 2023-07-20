2fd0456fd237b9d6a896e1a939bff7cda21915de
TinhBQ <81704759+TinhBQ@users.noreply.github.com>
2023-07-20 23:14:05 +0700

    GUI `section1` của homepage

--- src/assets/images/home/index.js
+++ src/assets/images/home/index.js
@@ -2,3 +2,4 @@ export { default as pic1 } from './pic1.jpg';
 export { default as pic2 } from './pic2.jpg';
 export { default as pic3 } from './pic3.png';
 export { default as pic4 } from './pic4.jpg';
+export { default as aboutStellar } from './about-stellar.jpg';
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,8 @@
 import Button from '../../components/Button';
 import classNames from 'classnames/bind';
 import styles from './Home.module.scss';
-import { pic1, pic2, pic3, pic4 } from '../../assets/images/home';
-import { Carousel, Container } from 'react-bootstrap';
+import { pic1, pic2, pic3, pic4, aboutStellar } from '../../assets/images/home';
+import { Carousel, Container, Row, Col } from 'react-bootstrap';
 
 const cx = classNames.bind(styles);
 
@@ -10,35 +10,78 @@ function Home() {
     const images = [pic1, pic2, pic3, pic4];
 
     return (
-        <div className={cx('hero')}>
-            <Carousel className={cx('item-wrapper')} controls={false} wrap={true} indicators={false} interval={10000}>
-                {images.map((image, index) => (
-                    <Carousel.Item key={index} className={cx('item')}>
-                        <div
-                            style={{
-                                backgroundImage: `url(${image})`,
-                                backgroundSize: 'cover',
-                                backgroundPosition: 'center',
-                                backgroundRepeat: 'no-repeat',
-                                maxWidth: '100%',
-                                height: '100%',
-                                filter: 'brightness(45%)',
-                            }}
-                        ></div>
-                    </Carousel.Item>
-                ))}
-            </Carousel>
-            <Container fluid="md">
-                <div className={cx('content-wrapper')}>
-                    <strong className={cx('titile')}>{`20+ NĂM UY TÍN TRONG\nLĨNH VỰC NHÀ HÀNG - KHÁCH SẠN`}</strong>
+        <>
+            <div className={cx('hero')}>
+                <Carousel
+                    className={cx('item-wrapper')}
+                    controls={false}
+                    wrap={true}
+                    indicators={false}
+                    interval={10000}
+                >
+                    {images.map((image, index) => (
+                        <Carousel.Item key={index} className={cx('item')}>
+                            <div
+                                style={{
+                                    backgroundImage: `url(${image})`,
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center',
+                                    backgroundRepeat: 'no-repeat',
+                                    maxWidth: '100%',
+                                    height: '100%',
+                                    filter: 'brightness(45%)',
+                                }}
+                            ></div>
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+                <Container fluid="md">
+                    <div className={cx('content-wrapper')}>
+                        <strong
+                            className={cx('titile')}
+                        >{`20+ NĂM UY TÍN TRONG\nLĨNH VỰC NHÀ HÀNG - KHÁCH SẠN`}</strong>
 
-                    <h3>Thực hiện phương châm kinh doanh: Trải nghiệm mới với hoài niệm cũ.</h3>
-                    <Button filled_1 className={cx('btn')}>
-                        TÌM HIỂU THÊM
-                    </Button>
-                </div>
-            </Container>
-        </div>
+                        <h3>Thực hiện phương châm kinh doanh: Trải nghiệm mới với hoài niệm cũ.</h3>
+                        <Button filled_1 className={cx('btn')}>
+                            TÌM HIỂU THÊM
+                        </Button>
+                    </div>
+                </Container>
+            </div>
+
+            <div className={cx('section-1')}>
+                <Container fluid="md" className="section-wrapper">
+                    <Row>
+                        <div className={cx('heading-wrapper')}>
+                            <p className={cx('titile')}>
+                                Phong cách huyền thoại làm cho nó trở thành một trong những khách sạn uy tín nhất.
+                            </p>
+                            <h1>MỘT ỐC ĐẢO THANH BÌNH Ở TRUNG TÂM THÀNH PHỐ CỦA BẠN</h1>
+                        </div>
+                    </Row>
+                    <Row className={cx('content')}>
+                        <Col md="6" className={cx('left')}>
+                            <p>
+                                Khách sạn này nằm trên một "ốc đảo" giữa lòng thành phố Hồ Chí Minh, tạo nên một không
+                                gian yên bình và thoáng đãng giữa sự sôi động của thành phố. Được bao quanh bởi sông và
+                                cây xanh, khách sạn mang đến cho du khách một trải nghiệm nghỉ dưỡng độc đáo.
+                            </p>
+                            <p>
+                                Kiến trúc của khách sạn được thiết kế hài hòa và tinh tế, tạo điểm nhấn nổi bật trong
+                                không gian xanh mát. Các phòng nghỉ tiện nghi được trang bị đầy đủ các tiện ích để mang
+                                lại sự thoải mái và sự nghỉ ngơi tuyệt vời cho khách hàng. Từ các cửa sổ và ban công,
+                                khách hàng có thể ngắm nhìn toàn cảnh thành phố hoặc tận hưởng cảnh quan tuyệt đẹp của
+                                "ốc đảo".
+                            </p>
+                            <Button outline_1={true}>Về Stellar</Button>
+                        </Col>
+                        <Col md="6" className={cx('right')}>
+                            <img src={aboutStellar} alt="About Stellar" />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        </>
     );
 }
 
