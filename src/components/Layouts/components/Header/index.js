2c718832d26f1fb6354d220921ce4133d0346d7a
LeVanAnhDuc <levananhduc1804@gmail.com>
2023-07-21 14:46:15 +0700

    merge TinhBQ/develop/main voi develop 21/7

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,10 +1,12 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
 
+import React from 'react';
+
 const cx = classNames.bind(styles);
 
 function Header() {
     return <h1 className={cx('wrapper')}>Header</h1>;
 }
 
-export default Header;
+export default React.memo(Header);
