151fe30ed9d78446c3d133180769f4a60ae9f37e
Hau Le <20110278@student.hcmute.edu.vn>
2023-07-31 22:46:33 +0700

    lienhe

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,10 +1,8 @@
-
 import React from 'react';
 import anh1 from "../../assets/images/anh1.png";
 import anh3 from "../../assets/images/anh3.jpg";
 import anh4 from "../../assets/images/anh4.jpg";
 import anh6 from "../../assets/images/anh6.jpg";
-import anh2 from "../../assets/images/anh2.png";
 import { Zoom } from "react-slideshow-image";
 import Button from '../../components/Button';
 import classNames from 'classnames/bind';
@@ -20,11 +18,11 @@ const images = [anh1, anh3, anh4, anh6];
 const cx = classNames.bind(styles);
 const zoomOutProperties = {
   duration: 1000,
-  transitionDuration: 1000,
+  transitionDuration: 800,
   infinite: true,
-  indicators: true,
+  indicators: false,
   scale: 0.4,
-  arrows: true
+  arrows: false,
 };
 const Slideshow = () => {
     return (
@@ -41,17 +39,17 @@ const FormShow = () => {
   return( 
     <div>
     <form className={cx('form_1')}>
-      <label for="fname" style={{marginLeft:10,marginTop:5}}><b>Họ và Tên:</b></label>
+      <label htmlFor="fname" style={{marginLeft:10,marginTop:5}}><b>Họ và Tên:</b></label>
       <input type="text" style={{ width:470, height:54}}></input> 
       </form>
 
       <form className={cx('form_1')}>
-      <label for="fname"style={{marginLeft:10,marginTop:5}}><b>Email</b>:</label>
+      <label htmlFor="fname"style={{marginLeft:10,marginTop:5}}><b>Email</b>:</label>
       <input type="text"  style={{width:"500px", height:54}}></input> 
       </form>
  
       <form  className={cx('form_1')} >
-      <label for="fname"style={{marginLeft:10,marginTop:5}}><b>Message:</b></label>
+      <label htmlFor="fname"style={{marginLeft:10,marginTop:5}}><b>Message:</b></label>
       <input type="text" style={{width:475, height:159}}></input>
       </form>
       </div> 
@@ -83,8 +81,7 @@ const Room = () => {
           <h3>Điện thoại: (+84) 9465412XX</h3>
           <h3>Email: info@stellar.com.vn</h3>
           </a>
-          <img src={anh2} style={{width:"100%",textAlign:"center",marginTop:30}}></img>
-        
+          <iframe className={cx('iframe')} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3663.375736707316!2d106.76933281027874!3d10.850632389258081!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752763f23816ab%3A0x282f711441b6916f!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBTxrAgcGjhuqFtIEvhu7kgdGh14bqtdCBUaMOgbmggcGjhu5EgSOG7kyBDaMOtIE1pbmg!5e1!3m2!1svi!2s!4v1690684807626!5m2!1svi!2s"></iframe>
           </div>
         </Col>  
      </Row>  
--- src/pages/RestaurentAndBar/index.js
+++ src/pages/RestaurentAndBar/index.js
@@ -6,15 +6,113 @@ import Res1 from "../../assets/images/Res1.jpg";
 import {Container , Row, Col} from 'react-bootstrap';
 import classNames from 'classnames/bind';  
 import styles from './RestaurentAndBar.module.scss';
+import Buf1 from "../../assets/images/buffet.jpg";
+import Res2 from "../../assets/images/Res2.jpg";
+import Res3 from "../../assets/images/Res3.jpg";
+import Res4 from "../../assets/images/Res4.jpg";
+import Bar_2 from "../../assets/images/Bar_2.jpg";
+import Bar_3 from "../../assets/images/Bar_3.jpg";
+import Bar_4 from "../../assets/images/Bar_4.jpeg";
+import Bar_5 from "../../assets/images/Bar_5.jpg";
+
 
 const cx = classNames.bind(styles);
 
+const ResShow = () => {
+    return (
+        <div>
+        <Container >  
+            <Row>  
+                <Col className={cx('Col_1')}>
+                <img className={cx('image_Buf1')}  src={Buf1}></img>
+                </Col>
+                <Col className={cx('Col_2')}>
+                <h1 className={cx('head_col2')} ><b>NHÀ HÀNG BUFFET</b></h1>
+                <div className={cx('main1_col2')}>
+                <h3><i>Nơi hoàn hảo để tận hưởng sự sang trọng và thoải mái tuyệt đỉnh!</i></h3>
+                </div>
+                <div className={cx('main2_col2')}><hr></hr></div>
+                <div className={cx('main3_col2')}>
+                <h4>Tọa lạc tầng 5, với góc nhìn đẹp hướng ra đại lộ Nguyễn Huệ, Stellar Hotel nổi bật với hơn 60 món ăn của đồng bằng sông Cửu Long được chế biến tỉ mỉ của gồm mì, bánh khọt, gỏi cuốn, đậu hũ, chè, hột vịt lộn và nước mát cũng như các món hải sản nướng. Ngoài ra, thực khách còn được thưởng thức các buổi trình diễn nhạc cụ truyền thống trong khi thưởng thức bữa ăn. </h4>
+                </div>
+                <div className={cx('main4_col2')}>
+                <h4>Giờ mở cửa:</h4>    
+                <h4> + Bữa sáng: từ 6 giờ sáng đến 9 giờ sang hàng ngày.</h4>
+                <h4> + Ăn trưa: từ 11 giờ sáng đến 2 giờ chiều hàng ngày.</h4>
+                <h4> + Bữa tối: từ 6 giờ tối đến 9 giờ 30 chiều hàng phục.</h4>
+                </div>
+                </Col>
+            </Row>
+        </Container>
+        </div>
+    )
+}
+
+const ResImage = () => {
+    return (
+    <div>
+       <h1 className={cx('imageRes_header')}><b>HÌNH ẢNH</b></h1>
+       <div>
+       <img className={cx('imageRes')} src={Res3}></img>
+       <img className={cx('imageRes')} src={Res4}></img>
+       <img className={cx('imageRes')} src={Res2}></img>
+       </div>
+       <Button className={cx('ButRes')} outline_1 = {true}>ĐĂNG KÝ NGAY</Button>
+    </div>
+    )
+}
+
+const BarShow = () => {
+    return (
+        <div>
+        <Container >  
+            <Row>  
+                <Col className={cx('Col_1')}>
+                <img className={cx('image_Buf1')}  src={Bar_3}></img>
+                </Col>
+                <Col className={cx('Col_2')}>
+                <h1 className={cx('head_col2')} style={{marginTop:30}} ><b>STELLAR HOLTEL</b></h1>
+                <div className={cx('main1_col2')}>  
+                </div>
+                <div className={cx('main2_col2')}><hr></hr></div>
+                <div className={cx('main3_col3')}>
+                <h4>Stellar Hotel mang không khí của câu lạc bộ biển. Thường xuyên thu hút người nước ngoài ăn mặc bảnh bao và người dân địa phương đến đây giải trí thư giãn. Quán bar trên tầng thượng này được trang bị rất nhiều cây nhiệt đới, trang trí nội thất theo tông màu đen trắng. Sàn nhảy rộng và quầy bar đầy đủ dự trữ trên boong chính phục vụ đầy và nhiệt tình các khách hàng đến đây.</h4>
+                </div>
+                <div className={cx('main4_col3')}>
+                <h4>Với một cái nhìn tuyệt đẹp của trung tâm Sài Gòn. Chill Skybar là một trong những hộp đêm nổi tiếng nhất trong thành phố. Ở đây bạn sẽ chứng kiến một Sài Gòn cực kì đẹp với thưởng thức nhạc dance hay hip hop sôi động đầy hấp dẫn. Nhưng đồ uống ở đây là rất tốn kém, chi phí của một cocktail gần 20$. Bù vào đó đừng bỏ lở giờ hạnh phúc tuyệt vời 17:30 đến 20:00, các loại thức ăn và đồ uống sẽ được giảm một nửa giá.
+</h4>    
+                </div>
+                </Col>
+            </Row>
+        </Container>
+        </div>
+    )
+}
+
+const BarImage = () => {
+    return (
+    <div>
+       <h1 className={cx('imageRes_header')}><b>HÌNH ẢNH</b></h1>
+       <div>
+       <img className={cx('imageRes')} src={Bar_4}></img>
+       <img className={cx('imageRes')} src={Bar_5}></img>
+       <img className={cx('imageRes')} src={Bar_3}></img>
+       </div>
+       <Button className={cx('ButRes')} outline_1 = {true}>ĐĂNG KÝ NGAY</Button>
+    </div>
+    )
+}
 
 function RestaurentAndBar() {
     return (
-        <div>
-        <img className={cx('Image_header')} src={Res1}></img>
-        <h1>RestaurentAndBar</h1>
+        <div className={cx('hero')} >
+        <img className={cx('image_header')}  src={Res1}></img>
+        <ResShow></ResShow>
+        <ResImage></ResImage>
+        <h1 className={cx('head_col3')}>hahaaha</h1>
+        <img className={cx('image_header')}  src={Bar_2}></img>
+        <BarShow></BarShow>
+        <BarImage></BarImage>
         </div>
     )
 }
