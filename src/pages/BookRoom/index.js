7316e9abcf8d473e92cc0b8469eb273abc77c14d
NguyetQue14062002 <20110706@student.hcmute.edu.vn>
2023-08-07 14:07:22 +0700

    doi format

--- src/pages/BookRoom/index.js
+++ src/pages/BookRoom/index.js
@@ -6,126 +6,149 @@ import Modal from 'react-bootstrap/Modal';
 
 import Image from '../../components/Image';
 //import {InfoRoomRight, InfoRoomLeft} from './InfoRoom';
-import {Deluxe1,Deluxe2,Executive1,Executive2,Suite1, Suite2,Superior1,Superior2,pic1,pic2,pic3, introRoom} from "../../assets/images/bookroom";
+import {
+    Deluxe1,
+    Deluxe2,
+    Executive1,
+    Executive2,
+    Suite1,
+    Suite2,
+    Superior1,
+    Superior2,
+    pic1,
+    pic2,
+    pic3,
+    introRoom,
+} from '../../assets/images/bookroom';
 import styles from './BookRoom.module.scss';
 import Button from '../../components/Button';
 
 const cx = classNames.bind(styles);
 function BookRoom() {
     const images = [pic1, pic2, pic3];
-    const room=[
+    const room = [
         {
-            id:1,
-            name:'Phòng Superior Double Or Twin',
-            acreage:'23m2',
-            typebed:'Giường đôi hoặc 02 giường đơn',
-            capacity:'2 người lớn',
-            view:'Phòng không có cửa sổ',
-            description:'Phòng nghỉ Stellar - Superior Double Or Twin mang đến sự ấm cúng bởi nét duyên dáng của lịch sử và những tiện nghi hiện đại nhất. Với sàn nhà kết hợp gỗ và gạch ốp, giường đôi thoải mái và đồ nội thất trang nhã, tất cả tạo nên sự cân bằng giữa “cổ điển” và “hiện đại”.',
-           images:[ Superior1, Superior2], 
+            id: 1,
+            name: 'Phòng Superior Double Or Twin',
+            acreage: '23m2',
+            typebed: 'Giường đôi hoặc 02 giường đơn',
+            capacity: '2 người lớn',
+            view: 'Phòng không có cửa sổ',
+            description:
+                'Phòng nghỉ Stellar - Superior Double Or Twin mang đến sự ấm cúng bởi nét duyên dáng của lịch sử và những tiện nghi hiện đại nhất. Với sàn nhà kết hợp gỗ và gạch ốp, giường đôi thoải mái và đồ nội thất trang nhã, tất cả tạo nên sự cân bằng giữa “cổ điển” và “hiện đại”.',
+            images: [Superior1, Superior2],
         },
         {
-            id:2,
-            name:'Phòng Deluxe Double',
-            acreage:'23m2',
-            typebed:'Giường đôi',
-            capacity:'2 người lớn',
-            view:'Phòng không có cửa sổ',
-            description:'Phòng nghỉ Stellar - Deluxe Double, căn phòng ấm áp này mang lại sự hoàn hảo trong kỳ nghỉ tại Sài Gòn. Không gian làm việc kết nối với các tiện nghi phòng nghỉ, cùng giường ngủ với bộ chăn lông vũ êm ái, chất liệu cao cấp cùng dịch vụ phục vụ phòng tận nơi mang lại sự thoải mái và thư giãn tối đa',
-            images:[ Deluxe1, Deluxe2],
-
+            id: 2,
+            name: 'Phòng Deluxe Double',
+            acreage: '23m2',
+            typebed: 'Giường đôi',
+            capacity: '2 người lớn',
+            view: 'Phòng không có cửa sổ',
+            description:
+                'Phòng nghỉ Stellar - Deluxe Double, căn phòng ấm áp này mang lại sự hoàn hảo trong kỳ nghỉ tại Sài Gòn. Không gian làm việc kết nối với các tiện nghi phòng nghỉ, cùng giường ngủ với bộ chăn lông vũ êm ái, chất liệu cao cấp cùng dịch vụ phục vụ phòng tận nơi mang lại sự thoải mái và thư giãn tối đa',
+            images: [Deluxe1, Deluxe2],
         },
         {
-            id:3,
-            name:'Phòng Executive City View',
-            acreage:'40m2',
-            typebed:'02 Giường đơn',
-            capacity:'2 người lớn',
-            view:'Thành phố',
-            description:'Với hai cửa sổ lớn cho quang cảnh tuyệt vời nhìn ra thành phố, Phòng Executive City View mang lại cho quý khách một không gian thoáng đãng, rộng mở. được trang trí bằng sàn gỗ kết hợp gạch, những món đồ nội thất phảng phất phong cách Đông Dương kết hợp những tiện nghi hiện đại tạo nên một tổng thể hài hoà, đương đại mà quý khách có thể trải nghiệm để cảm nhận nét đẹp Sài Gòn chuẩn xác nhất.',
-            images:[Executive1, Executive2 ] 
-
+            id: 3,
+            name: 'Phòng Executive City View',
+            acreage: '40m2',
+            typebed: '02 Giường đơn',
+            capacity: '2 người lớn',
+            view: 'Thành phố',
+            description:
+                'Với hai cửa sổ lớn cho quang cảnh tuyệt vời nhìn ra thành phố, Phòng Executive City View mang lại cho quý khách một không gian thoáng đãng, rộng mở. được trang trí bằng sàn gỗ kết hợp gạch, những món đồ nội thất phảng phất phong cách Đông Dương kết hợp những tiện nghi hiện đại tạo nên một tổng thể hài hoà, đương đại mà quý khách có thể trải nghiệm để cảm nhận nét đẹp Sài Gòn chuẩn xác nhất.',
+            images: [Executive1, Executive2],
         },
         {
-            id:4,
-            name:'Phòng Suite Garden',
-            acreage:'40m2',
-            typebed:'01 giường queen',
-            capacity:'2 người lớn',
-            view:'Thành phố',
-            description:'Phòng Suite Garden được phối hợp phong cách hiện đại với cảm hứng từ cây xanh, rộng rãi, hoàn hảo cho các kì nghỉ cuối tuần hay chuyến khám phá của quý khách. Ban công rộng và được sắp xếp để quý khách luôn cảm nhận được không khí trong lành, gió nhẹ lay và bóng mát từ các tán cây. Loại phòng nghỉ này đáp ứng đầy đủ nhu cầu ngắm nhìn đường phố, tận hưởng những giây phút đắm mình trong bồn tắm bể sục.',
-            images:[ Suite1, Suite2 ] 
-
-        }
-    ]
+            id: 4,
+            name: 'Phòng Suite Garden',
+            acreage: '40m2',
+            typebed: '01 giường queen',
+            capacity: '2 người lớn',
+            view: 'Thành phố',
+            description:
+                'Phòng Suite Garden được phối hợp phong cách hiện đại với cảm hứng từ cây xanh, rộng rãi, hoàn hảo cho các kì nghỉ cuối tuần hay chuyến khám phá của quý khách. Ban công rộng và được sắp xếp để quý khách luôn cảm nhận được không khí trong lành, gió nhẹ lay và bóng mát từ các tán cây. Loại phòng nghỉ này đáp ứng đầy đủ nhu cầu ngắm nhìn đường phố, tận hưởng những giây phút đắm mình trong bồn tắm bể sục.',
+            images: [Suite1, Suite2],
+        },
+    ];
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
     return (
-        <> 
-        <div className={cx('hero')}>
-            <Carousel
-                className={cx('item-wrapper')}
-                controls={false}
-                wrap={true}
-                indicators={false}
-                interval={3000}
-            >
-                {images.map((image, index) => (
-                    <Carousel.Item key={index} className={cx('item')}>
-                        <div
-                            style={{
-                                backgroundImage: `url(${image})`,
-                                backgroundSize: 'cover',
-                                backgroundPosition: 'center',
-                                backgroundRepeat: 'no-repeat',
-                                maxWidth: '100%',
-                                height: '100%',
-                            }}
-                        ></div>
-                    </Carousel.Item>
-                ))}
-            </Carousel>
-            <Container fluid="md">
+        <>
+            <div className={cx('hero')}>
+                <Carousel
+                    className={cx('item-wrapper')}
+                    controls={false}
+                    wrap={true}
+                    indicators={false}
+                    interval={3000}
+                >
+                    {images.map((image, index) => (
+                        <Carousel.Item key={index} className={cx('item')}>
+                            <div
+                                style={{
+                                    backgroundImage: `url(${image})`,
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center',
+                                    backgroundRepeat: 'no-repeat',
+                                    maxWidth: '100%',
+                                    height: '100%',
+                                }}
+                            ></div>
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+                <Container fluid="md">
                     <div className={cx('content-wrapper')}>
                         <div className={cx('input-date')}>
-                        <span className={cx('date')}>Check in   
-                        <input type='date' ></input>
-                        </span>
-                       <span className={cx('date')}>Check out   
-                        <input type='date'></input>
-                        </span>
+                            <span className={cx('date')}>
+                                Check in
+                                <input type="date"></input>
+                            </span>
+                            <span className={cx('date')}>
+                                Check out
+                                <input type="date"></input>
+                            </span>
                         </div>
-                      
-                        <Button  to={'/xem-gia'}
-                                 className={cx('btn')}>
-                        CHECK AVAILABILITY
+
+                        <Button to={'/xem-gia'} className={cx('btn')}>
+                            CHECK AVAILABILITY
                         </Button>
                     </div>
                 </Container>
             </div>
-            <Container fluid="md" >
+            <Container fluid="md">
                 <Row className={cx('Intro')}>
-                    <Col >
-                    <div className={cx('ContentIntro')}>
-                    <h1>Phòng nghỉ</h1>
+                    <Col>
+                        <div className={cx('ContentIntro')}>
+                            <h1>Phòng nghỉ</h1>
                             <p>Nơi hoàn hảo để tận hưởng sự sang trọng và thoải mái tuyệt đỉnh!</p>
-                            <hr  width="30%" align="center" />
-                            <p>Với vị trí trung tâm tại trái tim Sài Gòn, Stellar Hotel không chỉ mang đến sự thuận tiện trong việc di chuyển và khám phá các điểm tham quan nổi tiếng của thành phố, mà còn tạo nên một không gian yên tĩnh và thanh bình giữa sự sôi động của đô thị.</p>
-                            <p>Kiến trúc độc đáo và thiết kế tinh tế, Stellar Hotel đem đến cho quý khách không gian sống đẳng cấp và đầy phong cách. Từ các phòng nghỉ sang trọng đến các suite cao cấp, mỗi không gian được trang bị tiện nghi hiện đại và những chi tiết tỉ mỉ để mang đến sự thoải mái tối đa cho khách hàng</p>
-                    </div>
+                            <hr width="30%" align="center" />
+                            <p>
+                                Với vị trí trung tâm tại trái tim Sài Gòn, Stellar Hotel không chỉ mang đến sự thuận
+                                tiện trong việc di chuyển và khám phá các điểm tham quan nổi tiếng của thành phố, mà còn
+                                tạo nên một không gian yên tĩnh và thanh bình giữa sự sôi động của đô thị.
+                            </p>
+                            <p>
+                                Kiến trúc độc đáo và thiết kế tinh tế, Stellar Hotel đem đến cho quý khách không gian
+                                sống đẳng cấp và đầy phong cách. Từ các phòng nghỉ sang trọng đến các suite cao cấp, mỗi
+                                không gian được trang bị tiện nghi hiện đại và những chi tiết tỉ mỉ để mang đến sự thoải
+                                mái tối đa cho khách hàng
+                            </p>
+                        </div>
                     </Col>
                     <Col>
-                    <Image className={cx('IntroImage')} src={introRoom} alt="introRoom" />
+                        <Image className={cx('IntroImage')} src={introRoom} alt="introRoom" />
                     </Col>
                 </Row>
-            </Container> 
+            </Container>
             <div className={cx('Room')}>
-              {
-                    room.map((item, index) => (
-                        <Container className={cx('container')} fluid="md" key={item.id}>
-                        <Row className={cx('row')} style={{ flexDirection: item.id % 2 === 0 ? 'row-reverse' : 'row' }} >
+                {room.map((item, index) => (
+                    <Container className={cx('container')} fluid="md" key={item.id}>
+                        <Row className={cx('row')} style={{ flexDirection: item.id % 2 === 0 ? 'row-reverse' : 'row' }}>
                             <Col>
                                 <Carousel
                                     className={cx('item-wrapper')}
@@ -133,25 +156,24 @@ function BookRoom() {
                                     wrap={true}
                                     indicators={false}
                                     interval={3000}
-                                > 
-                                {
-                                    item.images.map((image, index) => (
+                                >
+                                    {item.images.map((image, index) => (
                                         <Carousel.Item key={index} className={cx('item')}>
-                                        <div
-                                            style={{
-                                            backgroundImage: `url(${image})`,
-                                            backgroundSize: 'cover',
-                                            backgroundPosition: 'center',
-                                            backgroundRepeat: 'no-repeat',
-                                            maxWidth: '100%',
-                                            height: '100%',
-                                            }}
-                                        ></div>
+                                            <div
+                                                style={{
+                                                    backgroundImage: `url(${image})`,
+                                                    backgroundSize: 'cover',
+                                                    backgroundPosition: 'center',
+                                                    backgroundRepeat: 'no-repeat',
+                                                    maxWidth: '100%',
+                                                    height: '100%',
+                                                }}
+                                            ></div>
                                         </Carousel.Item>
-                                     )) }
-                            </Carousel>
+                                    ))}
+                                </Carousel>
                             </Col>
-                            <Col >
+                            <Col>
                                 <h1>{item.name}</h1>
                                 <hr width="30%" align="center" />
                                 <Row>
@@ -211,20 +233,19 @@ function BookRoom() {
                                 </div>
                                 <div className={cx('date')}>
                                     Người lớn:
-                                    <input type="number" min="0" max="5" placeholder='1' />
+                                    <input type="number" min="0" max="5" placeholder="1" />
                                     Trẻ em:
-                                    <input type="number" min="0" max="5" placeholder='1'/>
+                                    <input type="number" min="0" max="5" placeholder="1" />
                                 </div>
-                               <Button className={cx('btn')} to={'/xem-gia'}>
-                                 Đặt phòng
-                               </Button>
+                                <Button className={cx('btn')} to={'/xem-gia'}>
+                                    Đặt phòng
+                                </Button>
                             </div>
                         </Modal>
                     </Container>
                 ))}
             </div>
         </>
-       
     );
 }
 
--- src/pages/ConferenceEvents/index.js
+++ src/pages/ConferenceEvents/index.js
@@ -1,43 +1,60 @@
 import styles from './ConferenceEvents.module.scss';
 import classNames from 'classnames/bind';
 import { Carousel } from 'react-bootstrap';
-import {pic1, pic2, pic3, pic4} from '../../assets/images/conference';
+import { pic1, pic2, pic3, pic4 } from '../../assets/images/conference';
 import Button from '../../components/Button';
 
 const cx = classNames.bind(styles);
 function ConferenceEvents() {
     const images = [pic1, pic2, pic3, pic4];
-    return <>
-          <div className={cx('hero')}>
-          <h1><strong>TRUNG TÂM HỘI NGHỊ</strong></h1>
-            <Carousel
-                className={cx('item-wrapper')}
-                controls={false}
-                wrap={true}
-                indicators={false}
-                interval={3000}
-            >
-                {images.map((image, index) => (
-                    <Carousel.Item key={index} className={cx('item')}>
-                        <div
-                            style={{
-                                backgroundImage: `url(${image})`,
-                                backgroundSize: 'cover',
-                                backgroundPosition: 'center',
-                                backgroundRepeat: 'no-repeat',
-                                maxWidth: '100%',
-                                height: '100%',
-                            }}
-                        ></div>
-                    </Carousel.Item>
-                ))}
-            </Carousel>
+    return (
+        <>
+            <div className={cx('hero')}>
+                <h1>
+                    <strong>TRUNG TÂM HỘI NGHỊ</strong>
+                </h1>
+                <Carousel
+                    className={cx('item-wrapper')}
+                    controls={false}
+                    wrap={true}
+                    indicators={false}
+                    interval={3000}
+                >
+                    {images.map((image, index) => (
+                        <Carousel.Item key={index} className={cx('item')}>
+                            <div
+                                style={{
+                                    backgroundImage: `url(${image})`,
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center',
+                                    backgroundRepeat: 'no-repeat',
+                                    maxWidth: '100%',
+                                    height: '100%',
+                                }}
+                            ></div>
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
             </div>
             <div className={cx('content-wrapper')}>
-            <hr width="20%" align="center"  />
-            <p>Trung tâm hội nghị Stellar chính là lựa chọn đẳng cấp dành cho các doanh nghiệp được thiết kế với phong cách tinh tế và sang trọng.</p>
-            <p>Stellar là một trung tâm tổ chức hội nghị đẳng cấp quốc tế tại TP.Hồ Chí Minh với lối kiến trúc tinh tế, độc đáo cùng một không gian rộng lớn, chất lượng dịch vụ chuyên nghiệp đẳng cấp hàng đầu. Được phát triển bởi đội ngũ nhân viên quản lý chuyên nghiệp được đào tạo bào bản theo đẳng cấp quốc tế nhằm mang đến sự hài lòng cho khách hàng.</p>
-            <p>Để đáp ứng những yêu cầu của khách hàng, chất lượng phục vụ, những mong muốn, nhu cầu về sản phẩm đồng thời thắt chặt mối quan hệ giữa khách hàng và doanh nghiệp, giữa đồng nghiệp với nhau. Việc tổ chức hội nghị hay các lễ kỷ niệm, tri ân khách hàng luôn đóng một vai trò rất quan trọng đối với mỗi doanh nghiệp. Đây cũng là cơ hội để doanh nghiệp quảng bá thương hiệu của mình rộng rãi, thu hút thêm được một lượng lớn khách hàng tiềm năng.</p>
+                <hr width="20%" align="center" />
+                <p>
+                    Trung tâm hội nghị Stellar chính là lựa chọn đẳng cấp dành cho các doanh nghiệp được thiết kế với
+                    phong cách tinh tế và sang trọng.
+                </p>
+                <p>
+                    Stellar là một trung tâm tổ chức hội nghị đẳng cấp quốc tế tại TP.Hồ Chí Minh với lối kiến trúc tinh
+                    tế, độc đáo cùng một không gian rộng lớn, chất lượng dịch vụ chuyên nghiệp đẳng cấp hàng đầu. Được
+                    phát triển bởi đội ngũ nhân viên quản lý chuyên nghiệp được đào tạo bào bản theo đẳng cấp quốc tế
+                    nhằm mang đến sự hài lòng cho khách hàng.
+                </p>
+                <p>
+                    Để đáp ứng những yêu cầu của khách hàng, chất lượng phục vụ, những mong muốn, nhu cầu về sản phẩm
+                    đồng thời thắt chặt mối quan hệ giữa khách hàng và doanh nghiệp, giữa đồng nghiệp với nhau. Việc tổ
+                    chức hội nghị hay các lễ kỷ niệm, tri ân khách hàng luôn đóng một vai trò rất quan trọng đối với mỗi
+                    doanh nghiệp. Đây cũng là cơ hội để doanh nghiệp quảng bá thương hiệu của mình rộng rãi, thu hút
+                    thêm được một lượng lớn khách hàng tiềm năng.
+                </p>
             </div>
             <div className={cx('session-1')}>
                 <div className={cx('content')}>
@@ -45,13 +62,12 @@ function ConferenceEvents() {
                     <h3>Điền thông tin liên hệ đặt hội nghị - sự kiện để được tư vấn và nhận giá tốt nhất</h3>
                     <input className={cx('name')} type="text" placeholder="Họ và tên" />
                     <input className={cx('email')} type="text" placeholder="Email" />
-                    <textarea name="subject" className={cx('email','mess')} placeholder="Lời nhắn..." ></textarea>
-                    <Button filled_1 >Gửi thông tin</Button>
+                    <textarea name="subject" className={cx('email', 'mess')} placeholder="Lời nhắn..."></textarea>
+                    <Button filled_1>Gửi thông tin</Button>
                 </div>
-             
             </div>
-    
-    </>
+        </>
+    );
 }
 
 export default ConferenceEvents;
